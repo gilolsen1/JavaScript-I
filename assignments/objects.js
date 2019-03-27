7b9ae9bcd1a99f7c93b0a9d4d750c7e1e951dc2f
Gilbert Olsen <gilbertolsen@Gilberts-MacBook-Pro.local>
2019-03-27 00:20:29 -0400

    object task 1 completed. created, logged and checked in console

--- assignments/objects.js
+++ assignments/objects.js
@@ -1,7 +1,9 @@
 // Let's get some practice writing a few objects for a new group of interns at a small business.
 
 // ==== Challenge 1: Writing Objects ==== 
-// HR needs some information on the new interns put into a database.  Given an id, email, first name, and gender. Create an object for each person in the company list:
+// HR needs some information on the new interns put into a database.  
+//Given an id, email, first name, and gender. 
+//Create an object for each person in the company list:
 
 // 1,mmelloy0@psu.edu,Mitzi,F
 // 2,kdiben1@tinypic.com,Kennan,M
@@ -18,20 +20,58 @@ const example = {
 }
 
 // Write your intern objects here:
+const Mitzi ={
+  "id":1,
+  "name":"Mitzi",
+  "email":"mmelloy0@psu.edu",
+  "gender":"F",
+}
+
+const Kennan = {
+  "id":2,
+  "name": "Kennan",
+  "email": "kdiben1@tinypic.com",
+  "gender": "M",
+}
+
+const Keven = {
+  "id": 3,
+  "name":"Keven",
+  "email":"kmummery2@wikimedia.org",
+  "gender":"M",
+}
+
+const Gannie = {
+  "id": 4,
+  "name":"Gannie",
+  "email":"gmartinson3@illinois.edu",
+  "gender":"F",
+}
+
+const Antonietta = {
+  "id": 5,
+  "name":"Antonietta",
+  "email":"adaine5@samsung.com",
+  "gender":"F",
+}
+
+
 
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
+console.log(Mitzi.name);
 
 // Kennan's ID
-
+console.log("Kennan's ID is " + Kennan.id);
 // Keven's email
-
+console.log("Keven's email is " + Keven.email);
 // Gannie's name
-
+console.log("Gannie's name is " + Gannie.name);
 // Antonietta's Gender
+console.log("Antonietta's Gender is " + Antonietta.gender);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
